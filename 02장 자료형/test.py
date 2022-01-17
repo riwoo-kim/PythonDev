787194066d163827e22616eb6d8c1b079858899a
rulu <riwoo021001@gmail.com>
2022-01-17 16:58:28 +0900

    20220117 16:58

--- 02장 자료형/test.py
+++ 02장 자료형/test.py
@@ -1,1 +1,10 @@
-print("Python Progaming")
+name = input("이름를 입력하세요: ")
+kor = input("국어 점수를 입력하세요: ")
+eng = input("영어 점수를 입력하세요: ")
+math = input("수학 점수를 입력하세요: ")
+
+total = int(kor) + int(eng) + int(math)
+avg = total /3
+
+print(name, "님의 총점=" , total, "평균=" , avg, "입니다." )
+#print(name)
